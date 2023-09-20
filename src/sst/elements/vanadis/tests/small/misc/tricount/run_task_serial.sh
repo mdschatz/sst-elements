7f52c7d3b7014fa79498143987f33e2b0083088a
Martin Daniel Schatz <mschatz@sandia.gov>
2023-09-20 14:48:38 -0600

    Added task_serial source

---
+++ src/sst/elements/vanadis/tests/small/misc/tricount/run_task_serial.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+TARGET=task_serial
+TASK_ARGS="$1_$2_$3_$4_$5_$6_$7"
+HFILE=configs/input_$TASK_ARGS.h
+CCFILE=configs/input_$TASK_ARGS.cc
+
+# Remove initial template file if they exist
+if [ -f lutArrays.h ] && [ ! -L lutArrays.h ]; then
+  rm lutArrays.h
+fi
+
+if [ -f lutArrays.cc ] && [ ! -L lutArrays.cc ]; then
+  rm lutArrays.cc
+fi
+
+# Ensure compilation files exist
+if [ ! -f $HFILE ]; then
+  echo "Header $HFILE does not exist"
+  exit
+fi
+
+if [ ! -f $CCFILE ]; then
+  echo "Source $CCFILE does not exist"
+  exit
+fi
+
+# Point symlinks to the correct input file
+if [ -L lutArrays.h ]; then
+  unlink lutArrays.h
+fi
+ln -s configs/input_$TASK_ARGS.h lutArrays.h
+
+if [ -L lutArrays.cc ]; then
+  unlink lutArrays.cc
+fi
+ln -s configs/input_$TASK_ARGS.cc lutArrays.cc
+
+# Ensure output directory exists
+if [ ! -d "./outs/" ]; then
+  mkdir -p ./outs
+fi
+
+
+export VANADIS_EXE=$PWD/$TARGET.riscv64
+export VANADIS_EXE_ARGS=${@: 8}
+echo ${VANADIS_EXE_ARGS}
+# Build executable
+make $TARGET
+#{ time sst perlmutter.py | tee outs/scaling_$2.out ; } 2> outs/time_scaling_$2.out
+time sst perlmutter.py
+
+# Build executable with logging and detailed timing
+#make CFLAGS=-DLOG tricount_clean_preload
+#{ time sst perlmutter.py ; } 2> outs/time_log_scaling_$2.out && mv stdout-100 outs/log_scaling_$2.out
--- src/sst/elements/vanadis/tests/small/misc/tricount/task_serial.cc
+++ src/sst/elements/vanadis/tests/small/misc/tricount/task_serial.cc
@@ -5,17 +5,19 @@
 #include <fcntl.h>
 #include <sys/time.h>
 
+#include "lutArrays.h"
+
 /* Diff from current: a subset of j vertices rather than just one
 */
-uint64_t doTask(uint64_t i, uint64_t first_i, uint64_t first_j, uint64_t last_j, uint64_t* index, uint64_t* edges) {
+uint64_t doTask(uint64_t i, uint64_t first_i, uint64_t first_j, uint64_t last_j, const uint64_t* index, const uint64_t* edges) {
     uint64_t tct = 0;
     
-    uint64_t* edges_i = &edges[index[first_i]]; // Start index for the edges i
-    uint64_t* edges_j = &edges[index[first_j]]; // Start index for the edges j
+    const uint64_t* edges_i = &edges[index[first_i]]; // Start index for the edges i
+    const uint64_t* edges_j = &edges[index[first_j]]; // Start index for the edges j
 
     uint64_t A = first_i + i;
-    uint64_t* first_A = edges_i + index[A];
-    uint64_t* last_A = edges_i + index[A+1];
+    const uint64_t* first_A = edges_i + (index[A] - index[first_i]);
+    const uint64_t* last_A = edges_i + (index[A+1] - index[first_i]);
 
     first_A = std::lower_bound(first_A, last_A, first_j); // first neighbor of A in paritition j
     if (first_A == last_A) return 0;
@@ -24,81 +26,45 @@ uint64_t doTask(uint64_t i, uint64_t first_i, uint64_t first_j, uint64_t last_j,
         uint64_t B = *first_A;
         if (B >= last_j) break; // all the rest of the neighbors are outside partition j
         
-        uint64_t * this_A = first_A + 1;
-        uint64_t * this_B = edges_j + index[B];
-        uint64_t * last_B = edges_j + index[B+1];
+        const uint64_t * this_A = first_A + 1;
+        const uint64_t * this_B = edges_j + (index[B] - index[first_j]);
+        const uint64_t * last_B = edges_j + (index[B+1] - index[first_j]);
 
         while ((this_A < last_A) && (this_B < last_B)) {
             if (*this_A < *this_B) {
-            this_A++;
+              this_A++;
             } else if (*this_A > *this_B) {
-                this_B++;
+              this_B++;
             } else {
-                this_A++;
-                this_B++;
-                tct++;
+              this_A++;
+              this_B++;
+              tct++;
             }
         }   
     }
     return tct;
-
-    /*
-uint64_t al, ae, at, a, b, bl;
-
-  //asm volatile ("rdcycle %0" : "=r" (cycles));
-  // going over all of the vertices: i is the vertex number
-  tct = 0; // Triangle count
-  al = index[index_i+1]; ae = al - 1;
-  at = index[index_i];
-  // at is the pointer into the edges array for i
-  b = index[edges[index_j]]; bl = index[edges[index_j]+1];
-  a = index_j + 1;
-  
-  // now search through a edges for match with b edges, a triangle
-  while(a < al && b < bl) {
-    if (edges[a] < edges[b]) {
-        a++;
-    } else if (edges[a] > edges[b]) {
-        b++;
-    } else {
-        a++; b++; tct++;
-    }
-  }
-  //asm volatile ("rdcycle %0" : "=r" (cycles));
-
-    return tct;*/
 }
+
 int main(int argc, char *argv[]) {
   uint64_t tct;
 
-  if (argc != 6) {printf("Usage: <graphname> <first_i> <first_j> <last_j> <i>\n"); return 1;}
+  if (argc != 5) {printf("Usage: <first_i> <first_j> <last_j> <i>\n"); return 1;}
 
-  std::string fn = argv[1];
   unsigned int cycles;
-  //static FILE* fp; // should work now, there was a bug in Vanadis
-  //fp = fopen(fn.c_str(), "r");
-  int fp = open( fn.c_str(), O_RDONLY|O_CREAT, S_IRWXU );
-  if (!fp) {
-      printf("%s() open '%s' failed\n",__func__, fn);
-      exit(-1);
-  }
-  uint64_t first_i = std::stoll(argv[2]);
-  uint64_t first_j = std::stoll(argv[3]);
-  uint64_t last_j = std::stoll(argv[4]);
-  uint64_t i = std::stoll(argv[5]);
-  uint64_t num_vertices;
-  uint64_t num_edges;
-  
-  read(fp, &num_vertices, sizeof(num_vertices));
-  read(fp, &num_edges, sizeof(num_edges));
-  
-  uint64_t* index = new uint64_t[num_vertices+1];
-  uint64_t *edges = new uint64_t[num_edges];
+  uint64_t first_i = std::stoll(argv[1]);
+  uint64_t first_j = std::stoll(argv[2]);
+  uint64_t last_j = std::stoll(argv[3]);
+  uint64_t i = std::stoll(argv[4]);
 
-  read(fp, index, sizeof(uint64_t)*(num_vertices+1));
-  read(fp, edges, sizeof(uint64_t)*num_edges);
-  //fclose(fp);
-  close(fp); 
+  uint64_t num_indices = indexSize;
+  const uint64_t *index = g_pIndex;
+  uint64_t num_edges = edgeSize;
+  const uint64_t *edges = g_pEdges;
+  uint64_t num_tasks = taskSize;
+  const uint64_t *tasks = g_pTasks;
+
+  uint64_t num_vertices = num_indices - 1;
+  
   printf("num_verticies %d\n", num_vertices);
   printf("num_edges %d\n",  num_edges);
 
