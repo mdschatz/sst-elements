d78d630bcefe6dbca58d3880b41e82c592a477c4
Martin Daniel Schatz <mschatz@sandia.gov>
2023-09-25 14:53:58 -0600

    Merged doCountTriangles logic across uses

--- src/sst/elements/vanadis/tests/small/misc/tricount/task_parallel.cc
+++ src/sst/elements/vanadis/tests/small/misc/tricount/task_parallel.cc
@@ -8,48 +8,9 @@
 #include <fcntl.h>
 
 #include "lutArrays.h"
+#include "taskUtil.h"
 
 using namespace std;
-
-uint64_t doCountTriangles(uint64_t i, uint64_t first_i, uint64_t first_j, uint64_t last_j, const uint64_t* index, const uint64_t* edges) {
-    uint64_t tct = 0;
-
-    const uint64_t* edges_i = &edges[index[first_i]]; // Start index for the edges i
-    const uint64_t* edges_j = &edges[index[first_j]]; // Start index for the edges j
-
-    uint64_t A = first_i + i;
-    const uint64_t* first_A = edges_i + (index[A] - index[first_i]);
-    const uint64_t* last_A = edges_i + (index[A+1] - index[first_i]);
-
-    first_A = std::lower_bound(first_A, last_A, first_j); // first neighbor of A in paritition j
-    if (first_A == last_A) return 0;
-
-    for ( ; first_A < last_A; first_A++) {
-        uint64_t B = *first_A;
-        if (B >= last_j) break; // all the rest of the neighbors are outside partition j
-
-        const uint64_t * this_A = first_A + 1;
-        const uint64_t * this_B = edges_j + (index[B] - index[first_j]);
-        const uint64_t * last_B = edges_j + (index[B+1] - index[first_j]);
-
-        while ((this_A < last_A) && (this_B < last_B)) {
-            if (*this_A < *this_B) {
-              this_A++;
-            } else if (*this_A > *this_B) {
-              this_B++;
-            } else {
-              // (Parallel) Debug printing
-              // omp_set_lock( &lock );
-              // printf("[%u] triangle at: (i, j, k)=(%d,%d,%d)\n", tid, first_i + i, B, *this_A);
-              // omp_unset_lock( &lock );
-              this_A++;
-              this_B++;
-              tct++;
-            }
-        }
-    }
-    return tct;
-}
 
 int main(int argc, char *argv[]) {
 
--- src/sst/elements/vanadis/tests/small/misc/tricount/task_serial.cc
+++ src/sst/elements/vanadis/tests/small/misc/tricount/task_serial.cc
@@ -6,44 +6,7 @@
 #include <sys/time.h>
 
 #include "lutArrays.h"
-
-/* Diff from current: a subset of j vertices rather than just one
-*/
-uint64_t doTask(uint64_t i, uint64_t first_i, uint64_t first_j, uint64_t last_j, const uint64_t* index, const uint64_t* edges) {
-    uint64_t tct = 0;
-    
-    const uint64_t* edges_i = &edges[index[first_i]]; // Start index for the edges i
-    const uint64_t* edges_j = &edges[index[first_j]]; // Start index for the edges j
-
-    uint64_t A = first_i + i;
-    const uint64_t* first_A = edges_i + (index[A] - index[first_i]);
-    const uint64_t* last_A = edges_i + (index[A+1] - index[first_i]);
-
-    first_A = std::lower_bound(first_A, last_A, first_j); // first neighbor of A in paritition j
-    if (first_A == last_A) return 0;
-
-    for ( ; first_A < last_A; first_A++) {
-        uint64_t B = *first_A;
-        if (B >= last_j) break; // all the rest of the neighbors are outside partition j
-        
-        const uint64_t * this_A = first_A + 1;
-        const uint64_t * this_B = edges_j + (index[B] - index[first_j]);
-        const uint64_t * last_B = edges_j + (index[B+1] - index[first_j]);
-
-        while ((this_A < last_A) && (this_B < last_B)) {
-            if (*this_A < *this_B) {
-              this_A++;
-            } else if (*this_A > *this_B) {
-              this_B++;
-            } else {
-              this_A++;
-              this_B++;
-              tct++;
-            }
-        }   
-    }
-    return tct;
-}
+#include "taskUtil.h"
 
 int main(int argc, char *argv[]) {
   uint64_t tct;
@@ -71,7 +34,7 @@ int main(int argc, char *argv[]) {
   struct timeval tv1;
   struct timeval tv2;
   gettimeofday(&tv1, NULL);
-  tct = doTask(i,first_i,first_j,last_j,index,edges);
+  tct = doCountTriangles(i,first_i,first_j,last_j,index,edges);
   gettimeofday(&tv2, NULL);
   tv1.tv_sec = tv2.tv_sec - tv1.tv_sec;
   tv1.tv_usec = tv2.tv_usec - tv1.tv_usec;
